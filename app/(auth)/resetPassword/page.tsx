b91f0730d5e5f74000c6d714ba4d33f6477123d8
NiteshCodes7 <workwithnites@gmail.com>
2025-07-26 01:16:03 +0530

    Added PDF uplaod feature through ZOD and uploadthing

--- app/(auth)/resetPassword/page.tsx
+++ app/(auth)/resetPassword/page.tsx
@@ -89,6 +89,7 @@ export default function resetPassword() {
             name="email"
             onChange={handleChange}
             value={form.email}
+            required
           />
         </div>
       </div>
