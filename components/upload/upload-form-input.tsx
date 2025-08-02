195d356672e51f9f8e932dc29775d305ca53f5c0
NiteshCodes7 <workwithnites@gmail.com>
2025-08-02 09:12:59 +0530

    bug fixed in upload form

--- components/upload/upload-form-input.tsx
+++ components/upload/upload-form-input.tsx
@@ -1,15 +1,54 @@
 "use client";
 
-import React, { forwardRef } from "react";
+import React, { forwardRef, useEffect, useState } from "react";
 import { Input } from "@/components/ui/input";
 import { Button } from "@/components/ui/button";
 import { Loader } from "lucide-react";
 import { MotionDiv } from "@/components/common/motion-wrapper";
+import { getUserPlan } from "@/lib/getUserPlan";
+import axios from "axios";
 
 type UploadFormInputProps = {
   onSubmit: (e: React.FormEvent<HTMLFormElement>) => void;
   isLoading: boolean;
 };
+
+const uploadLimit: Record<string, number> = {
+  free: 2,
+  basic: 10,
+  pro: Infinity,
+};
+
+const [summaries, setSummaries] = useState<any[]>([]);
+const [limit, setLimit] = useState<number>(2);
+
+useEffect(() => {
+  const checkLoginStatus = async () => {
+    try {
+      const res = await axios.get("/api/check-auth", {
+        withCredentials: true,
+      });
+      if (res.status === 200 && res.data.loggedIn) {
+        const email = res.data?.user?.email;
+        const id = res.data?.user?._id;
+
+        const summaryRes = await axios.post("/api/get-summary", { id });
+        if (summaryRes.status === 200) {
+          setSummaries(summaryRes.data?.summaries || []);
+        }
+
+        const userPlan = await getUserPlan(email);
+        setLimit(uploadLimit[userPlan]);
+      }
+    } catch (error) {
+      console.error("Error in upload form:", error);
+    }
+  };
+
+  checkLoginStatus();
+}, []);
+
+let isValid = summaries.length > limit;
 
 const UploadFormInput = forwardRef<HTMLFormElement, UploadFormInputProps>(
   ({ onSubmit, isLoading }, ref) => {
@@ -28,7 +67,7 @@ const UploadFormInput = forwardRef<HTMLFormElement, UploadFormInputProps>(
             accept="application/pdf"
             required
           />
-          <Button disabled={isLoading}>
+          <Button disabled={isValid || isLoading}>
             {isLoading ? (
               <>
                 <Loader className="animate-spin" /> Processing...
